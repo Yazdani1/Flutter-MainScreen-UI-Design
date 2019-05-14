2168d61a2152b5b2b7ba8c27c9dc0dfa895bef99
Yazdani <yaz4dani@gmail.com>
2019-05-14 21:22:02 +0600

    second

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -48,10 +48,7 @@ class _HomeState extends State<Home> {
                       ),
                     )
                   ],
-
-
-
-                    
+                )
               ],
             ),
           ),//end first container..
